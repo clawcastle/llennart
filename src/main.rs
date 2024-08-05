84a55c64aad202272e03d9c468388b5d0a3bee48
sunero4 <anderskloborg@gmail.com>
2024-08-05 18:19:56 +0000

    Add stub llm client that just returns 'answer' a bunch of times.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,52 @@
 use clap::Parser;
+use rand::{Rng, RngCore};
 
 #[derive(Parser, Debug)]
 struct Args {
-    
+
 }
 
 fn main() {
     let args = Args::parse();
+}
+
+trait LlmClient {
+    fn ask_question(&self, question: &str) -> String;
+}
+
+struct LlmClientStub;
+
+impl LlmClient for LlmClientStub {
+    fn ask_question(&self, question: &str) -> String {
+        let mut rng = rand::thread_rng();
+
+        let n: usize = rng.gen_range(20..200);
+
+        let mut result = String::new();
+
+        for i in 0..n {
+            result.push_str("answer ");
+
+            if i % 15 == 0 {
+                result.push_str("\n\n");
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ask_question_to_llm_client() {
+        let llm_client = LlmClientStub;
+
+        let answer = llm_client.ask_question("");
+
+        println!("{}", &answer);
+    }
+
 }
