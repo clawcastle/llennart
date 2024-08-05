505e440b642dcc66a93267a7225f3e9dbfda7634
sunero4 <anderskloborg@gmail.com>
2024-08-05 18:47:25 +0000

    Add structs for questions and answers.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use rand::Rng;
+use rand::{Rng, RngCore};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -10,17 +10,26 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    println!("Question asked: {}", &args.question);
+    let llm_client = LlmClientStub;
+
+    let question = Question::new(&args.question);
+
+    let answer = llm_client.ask_question(&question);
+
+    println!("Question asked (id: {}): '{}'.", &question.id, &question.question);
+
+    println!("Answer (id: {}): \n", &answer.id);
+    println!("{}\n", &answer.answer);
 }
 
 trait LlmClient {
-    fn ask_question(&self, question: &str) -> String;
+    fn ask_question(&self, question: &Question) -> Answer;
 }
 
 struct LlmClientStub;
 
 impl LlmClient for LlmClientStub {
-    fn ask_question(&self, question: &str) -> String {
+    fn ask_question(&self, question: &Question) -> Answer {
         let mut rng = rand::thread_rng();
 
         let n: usize = rng.gen_range(20..200);
@@ -34,8 +43,44 @@ impl LlmClient for LlmClientStub {
                 result.push_str("\n\n");
             }
         }
+        
+        Answer::new(&result)
+    }
+}
 
-        result
+struct Question {
+    id: String,
+    question: String
+}
+
+impl Question {
+    fn new(question: &str) -> Self {
+        let mut rand = rand::thread_rng();
+
+        let id = rand.next_u64().to_string();
+
+        Self {
+            id,
+            question: question.to_string()
+        }
+    }
+}
+
+struct Answer {
+    id: String,
+    answer: String
+}
+
+impl Answer {
+    fn new(answer: &str) -> Self {
+        let mut rand = rand::thread_rng();
+
+        let id = rand.next_u64().to_string();
+
+        Self {
+            id,
+            answer: answer.to_string()
+        }
     }
 }
 
@@ -47,9 +92,9 @@ mod tests {
     fn ask_question_to_llm_client() {
         let llm_client = LlmClientStub;
 
-        let answer = llm_client.ask_question("");
+        let answer = llm_client.ask_question(&Question::new("question"));
 
-        println!("{}", &answer);
+        println!("{}", &answer.answer);
     }
 
 }
